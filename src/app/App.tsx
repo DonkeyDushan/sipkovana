1238bad94d1e72e223f9d56b0a382541955efa51
Natálie Sedláková <natalie.sedlakova@betrian.cz>
2024-07-28 16:57:29 +0200

    fix: deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { StyledEngineProvider, CssBaseline } from '@mui/material';
-import './App.css';
+import { StyledEngineProvider } from '@mui/material';
 import { MainPage } from './pages';
+
+import './App.css';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <StyledEngineProvider injectFirst>
-        <CssBaseline />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </StyledEngineProvider>
-    </BrowserRouter>
+    <StyledEngineProvider injectFirst>
+      <MainPage />
+    </StyledEngineProvider>
   );
 };
